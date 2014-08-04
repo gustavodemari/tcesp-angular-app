82f3328ac36f5614d640350b20a600b04d04c68e
gustavodemari <guhdemari@gmail.com>
2014-08-04 20:00:48 -0300

    updating despesas route and view

--- app/modules/core.js
+++ app/modules/core.js
@@ -9,4 +9,7 @@ app.config(['$routeProvider', function($routeProvider){
   $routeProvider.when('/users', {templateUrl:'views/users.html', controller: 'UsersListCtrl'});
   $routeProvider.when('/users/:userId', {templateUrl:'views/users_detail.html', controller: 'UsersDetailCtrl'});
   $routeProvider.when('/despesas', {templateUrl:'views/despesas.html', controller: 'DespesasMunicipiosCtrl'});
+  $routeProvider.when('/despesas/:municipioId', {templateUrl:'views/despesas_detail.html', controller: 'DespesasMunicipiosDetailCtrl'});
+  $routeProvider.when('/despesas/:municipioId/:orgaoId/:ano', {templateUrl:'views/despesas_orgaos.html', controller: 'DespesasOrgaosCtrl'});
 }])
+
--- app/modules/custom/despesas.js
+++ app/modules/custom/despesas.js
@@ -4,7 +4,7 @@ var API_URL = 'http://localhost:5000/api';
 var DespesasSvc = angular.module('tcesp-angular-app.services.despesas', ['ngResource']);
 
 DespesasSvc.factory('Despesas', ['$resource', function($resource){
-  return $resource(API_URL + '/despesas/:municipioId', {}, {
+  return $resource(API_URL + '/despesas/:municipioId/:orgaoId/:ano/:tipoDespesa', {}, {
     list: {method: 'GET', isArray: true}
   });
 }]);
@@ -13,5 +13,21 @@ DespesasSvc.factory('Despesas', ['$resource', function($resource){
 var DespesasCtrl = angular.module('tcesp-angular-app.controllers.despesas', []);
 
 DespesasCtrl.controller('DespesasMunicipiosCtrl', ['$scope', 'Despesas', function($scope, Despesas){
-  $scope.despesas = 'teste';
+  $scope.despesas = Despesas.list();
 }]);
+
+DespesasCtrl.controller('DespesasMunicipiosDetailCtrl', ['$scope', '$routeParams', 'Despesas', function($scope, $routeParams, Despesas){
+  $scope.despesas = Despesas.list({municipioId: $routeParams.municipioId});
+}]);
+
+DespesasCtrl.controller('DespesasOrgaosCtrl', ['$scope', '$routeParams', 'Despesas', function($scope, $routeParams, Despesas){
+  $scope.anos = [2014, 2013, 2012, 2011, 2010, 2009, 2008];
+
+  $scope.despesas = Despesas.list({
+      municipioId: $routeParams.municipioId, 
+      orgaoId: $routeParams.orgaoId,
+      ano: $routeParams.ano
+    });
+}]);
+
+//http://jsfiddle.net/m6er5/
