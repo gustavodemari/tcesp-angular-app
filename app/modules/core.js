f59995a0b1715bf664c6e477cfb1f66553e119bb
gustavodemari <guhdemari@gmail.com>
2014-08-05 06:52:08 -0300

    adding rorgaos routes, services and controllers

--- app/modules/core.js
+++ app/modules/core.js
@@ -13,5 +13,6 @@ app.config(['$routeProvider', function($routeProvider){
   $routeProvider.when('/despesas/:municipioId', {templateUrl:'views/despesas_detail.html', controller: 'DespesasMunicipiosDetailCtrl'});
   $routeProvider.when('/despesas/:municipioId/:orgaoId', {templateUrl:'views/despesas_orgaos.html', controller: 'DespesasOrgaosCtrl'});
   $routeProvider.when('/despesas/:municipioId/:orgaoId/:ano', {templateUrl:'views/despesas_orgaos.html', controller: 'DespesasOrgaosCtrl'});
+  $routeProvider.when('/orgaos', {templateUrl:'views/orgaos_list.html', controller: 'OrgaosListCtrl'});
 }])
 
--- app/modules/custom/orgaos.js
+++ app/modules/custom/orgaos.js
@@ -10,3 +10,26 @@ OrgaosSvc.factory('Orgaos', ['$resource', function($resource){
 
 
 var OrgaosCtrl = angular.module('tcesp-angular-app.controllers.orgaos', []);
+
+OrgaosCtrl.controller('OrgaosListCtrl', ['$scope', '$window','Orgaos', function($scope, $window, Orgaos){
+  function getStoredOrgaos(){
+    var orgaosLocal = JSON.parse($window.localStorage.getItem("orgaos"));
+    var actualDate = (new Date());
+    if(orgaosLocal){
+      if(new Date(orgaosLocal.expirationDate) > actualDate){
+        return orgaosLocal.data;
+      }
+    }
+    return '';
+  }
+
+  function storeOrgaos(){
+    var expirationDate = (new Date());
+    expirationDate.setDate(expirationDate.getDate()+30);
+    var orgaosLocal = {expirationDate: expirationDate, data: $scope.orgaos};
+    $window.localStorage.setItem("orgaos", JSON.stringify(orgaosLocal));
+  }  
+
+  $scope.orgaos =  getStoredOrgaos() || Orgaos.list(storeOrgaos);
+
+}])
